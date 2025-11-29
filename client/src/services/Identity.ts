44ffee5a99c44d335f82b403865b328b32940ccf
Clark <clarkmillermail@gmail.com>
2025-11-29 12:09:15 -0500

    Updated client to use helios-identity-sdk package instead of manually building things out

--- client/src/services/Identity.ts
+++ client/src/services/Identity.ts
@@ -1,37 +1,14 @@
-import { buildHeaders, type FetcherData } from '@helpers/ApiHelper';
+import { type FetcherData } from '@helpers/ApiHelper';
+import { login as identityLogin, auth as identityAuth } from 'helios-identity-sdk/services/Identity/Auth';
 
 const IDENTITY_URL = import.meta.env.VITE_IDENTITY_URL;
 const IDENTITY_API_URL = import.meta.env.VITE_IDENTITY_API_URL ?? IDENTITY_URL;
-const BASE_URL = '/api/identity';
 
 export const auth = async (): Promise<FetcherData> => {
-	const payload: FetcherData = {};
-
-	try {
-		const url = new URL(`${IDENTITY_API_URL}${BASE_URL}/auth`);
-
-		const headers = buildHeaders();
-
-		const response = await fetch(url.toString(), { headers: headers });
-		if (response.status == 401) throw new Error('Unauthorized');
-		payload.data = await response.text();
-	} catch (err: unknown) {
-		const errorMessage: string = err instanceof Error ? err.message : 'Unknown error occured';
-		console.log('Error getting auth: ' + errorMessage);
-		payload.error = errorMessage;
-	}
-
-	return payload;
+	return await identityAuth(IDENTITY_API_URL);
 };
 
 // Try auth, if that doesn't work navigate to identity site giving window.location.href as return url
 export const login = async (optional: boolean = false): Promise<any> => {
-	const response = await auth();
-
-	if (!response.error) return response.data;
-	if (optional) return false;
-	const newUrl = new URL(IDENTITY_URL);
-	newUrl.searchParams.append('redirectUrl', window.location.href);
-
-	window.location.href = newUrl.toString();
+	return await identityLogin(IDENTITY_URL, { optional: optional, identityApiUrl: IDENTITY_API_URL });
 };
