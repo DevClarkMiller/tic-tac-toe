06c494df259226e9b8baa5d07ba0a067ff10ce9a
DevClarkMiller <clarkmillermail@gmail.com>
2025-12-03 21:56:52 -0500

    Added in a debug to Site Service

--- client/src/services/Site.ts
+++ client/src/services/Site.ts
@@ -4,6 +4,8 @@ import * as signalR from '@microsoft/signalr';
 const SITE_URL = window.location.hostname;
 const API_URL = import.meta.env.VITE_SITE_API_URL ?? SITE_URL;
 
+console.log(SITE_URL, API_URL);
+
 export const createSession = async (): Promise<FetcherData> => {
 	const data = await poster(`${API_URL}/api/session`, null);
 	return data;
