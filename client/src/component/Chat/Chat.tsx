7d0809a7980d72af0934d71b1747743247d3dea1
Clark <clarkmillermail@gmail.com>
2025-12-13 21:17:33 -0500

    Now auto scrolling when a new message comes in

--- client/src/component/Chat/Chat.tsx
+++ client/src/component/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import type { Message } from 'types/Message';
 import { SessionContext } from '@context/SessionContext';
 import { AppContext } from 'App';
@@ -12,6 +12,15 @@ const Chat = () => {
 	const { messages, sendMessage } = useContext(SessionContext);
 	const { user } = useContext(AppContext);
 
+	const messagesEndRef = useRef<HTMLElement | null>(null);
+
+	useEffect(() => {
+		if (!messagesEndRef.current) return;
+
+		console.log('SCROLLING');
+		messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
+	}, [messages]);
+
 	const [fontSizes, setFontSizes] = useState<FontControlType>({
 		messageHeader: MESSAGE_FONT_HEADER_DEFAULT,
 		messageContent: MESSAGE_FONT_CONTENT_DEFAULT,
@@ -20,14 +29,11 @@ const Chat = () => {
 	return (
 		<div className="w-100 rounded-2 p-1 pe-0" style={{ border: '1px solid gray' }}>
 			<ChatHeader fontSizes={fontSizes} setFontSizes={setFontSizes} />
-			<div className="overflow-y-scroll chat-container" data-bs-spy="scroll">
-				{messages.map((message: Message) => (
-					<UserMessage
-						fontSizes={fontSizes}
-						key={message.user + '-' + message + '-' + message.dateReceived}
-						message={message}
-					/>
+			<div className="overflow-y-scroll chat-container" data-bs-spy="scroll" ref={messagesEndRef}>
+				{messages.map((message: Message, idx: number) => (
+					<UserMessage fontSizes={fontSizes} key={message.user + '-' + idx} message={message} />
 				))}
+				<div ref={messagesEndRef as any} />
 			</div>
 			<MessageInput user={user!} sendMessage={sendMessage} />
 		</div>
